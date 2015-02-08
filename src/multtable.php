a7af820acc87cd575f912a1ac7a66eff20f74a5f
greg <gregorio.jp.mitchell@gmail.com>
2015-02-08 14:59:57 -0700

    table with unprotected _get requests and no error checking.

--- src/multtable.php
+++ src/multtable.php
@@ -1,16 +1,19 @@
 <?php
-$rows=10;
-$cols=10;
+$rows = $_GET['rows'];
+$cols=  $_GET['cols'];;
 
 echo "<table border='1'>";
-for ($tr=1;$tr<$rows;$tr++)
+for ($tr=1;$tr<=$rows;$tr++)
     {
         echo "<tr>";
-        for ($td=1;$td<$cols;$td++)
+        for ($td=1;$td<=$cols;$td++)
             {
                 echo "<td align='center' border='1'>".$tr*$td."</td>";
             }
         echo "</tr>";
     }
 echo "</table>";
+
+/* echo $rows; */
+
 ?>
