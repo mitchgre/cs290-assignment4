435c55a05545992b4afdf16bfb3c9c1ffc20b20f
greg <gregorio.jp.mitchell@gmail.com>
2015-02-08 18:54:34 -0700

    loopback.php queued up and ready to fill in.

---
+++ src/loopback.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML SYSTEM>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>loopback</title>
+    <style>
+     tr,td { align:center;
+	     border:solid;
+	     border-width: 1px;
+       }
+    </style>
+  </head>
+<body>
+<?php
+/*
+
+   This file should accept either a GET or POST for input. That GET or
+   POST will have an unknown number of key/value pairs. 
+
+   The page should return a JSON object (remember, this is almost
+   identical to an object literal in JavaScript) of the form 
+
+ {"Type":"[GET|POST]","parameters":{"key1":"value1",   ... ,"keyn":"valuen"}}. 
+
+   Behavior if a key is passed in and no value is specified is
+   undefined. If no key value pairs are passed it should return
+
+   {"Type":"[GET|POST]", "parameters":null}. You are welcome to use
+   built in JSON function in PHP to produce this output. 
+
+*/
+?>
+
+</body>
+</html>
