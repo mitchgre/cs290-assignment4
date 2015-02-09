ec35c092aa16fcdf57c6a1230e471c5d62605a15
greg <gregorio.jp.mitchell@gmail.com>
2015-02-09 00:13:50 -0700

    content1.php -> trying to overcome problems with session variables..

--- src/login.php
+++ src/login.php
@@ -46,6 +46,17 @@ login.php.
 */
 
 session_start();
+
+if (ini_get('register_globals'))
+{
+    foreach ($_SESSION as $key=>$value)
+    {
+        if (isset($GLOBALS[$key]))
+            unset($GLOBALS[$key]);
+    }
+}
+
+
 $_SESSION['n'] = 0;
 $_SESSION['m'] = 0;
 // output a form to login
